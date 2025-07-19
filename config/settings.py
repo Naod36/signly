ff950f89cb42d9f5d3b13ef2633f4f683d8cb14e
Naod <naodk36@gmail.com>
2025-07-19 23:48:45 +0300

    Add STATIC_ROOT for Render deploy

--- config/settings.py
+++ config/settings.py
@@ -121,3 +121,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+
+# Static files (CSS, JavaScript, etc.)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # ✅ This is required for Render
